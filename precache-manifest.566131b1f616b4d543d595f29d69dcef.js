faee64f89a95472503d449a431c2ef1bf4a5f848
s.tolkacheva <s.tolkacheva@systtech.ru>
2022-04-13 16:59:48 +0300

    Updates

---
+++ precache-manifest.566131b1f616b4d543d595f29d69dcef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0471943dc264be37ea174d4a9b26ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ef4c274cc5667d82161",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "a32d1e2123e619dcc940",
+    "url": "/static/css/main.bd9dd223.chunk.css"
+  },
+  {
+    "revision": "4ef4c274cc5667d82161",
+    "url": "/static/js/2.59883db0.chunk.js"
+  },
+  {
+    "revision": "a32d1e2123e619dcc940",
+    "url": "/static/js/main.c1f01ae0.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
